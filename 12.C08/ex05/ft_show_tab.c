dee9b9ccbf753b3dbb8af3282e84cfc44a2aa81d
EugeneCT <eugenelct917@gmail.com>
2023-07-06 12:04:07 +0800

    done c08 ex04 and ex05

--- 12.C08/ex05/ft_show_tab.c
+++ 12.C08/ex05/ft_show_tab.c
@@ -10,52 +10,68 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#include "ft_stock_str.h"
+#include "unistd.h"
 
-int	ft_strlen(char *str)
+void	mini_putnbr(int nb)
 {
-	int i = 0;
-	while (str[i] != '\0')
+	char	c;
+
+	if (nb < 0)
 	{
-		i++;
+		write(1, "-", 1);
+		nb = -nb;
 	}
-	return (i);
+	c = nb + '0';
+	write(1, &c, 1);
 }
 
-char* strcpy(char *str,int size)
+void	ft_putnbr(int nb)
 {
-	int i;
-	char *ret_str;
-	
-	i=0;
-	ret_str  = (char *)malloc((*size + 1) * sizeof(str));
-	if (!ret_str)
-		return (NULL);
-	while str[i] != '/0'
+	if (nb > -10 && nb < 10)
+		mini_putnbr(nb);
+	else if (nb > 0)
 	{
-		ret_str[i] = str[i];
-		i++:
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
 	}
-	ret_str[i] = '/0';
-	return (ret_str);
+	else
+	{
+		if (nb == -2147483648)
+			write(1, "-2147483648", 11);
+		else
+		{
+			write(1, "-", 1);
+			ft_putnbr(-nb);
+		}
+	}
 }
 
-struct s_stock_str *ft_strs_to_tab(int ac, char**av)
+void	ft_putstring(char *str)
 {
 	int	i;
-	t_stock_str *ret_list;
-	ret_list = malloc(sizeof(t_stock_str) * (ac + 1))
-	if (!ret_list)
-		return NULL
-	i = 0;
-	while (i < ac)
+
+	if (str)
 	{
-		ret_list[i].size=ft_strlen(av[i]);
-		ret_list[i].str = av[i];
-		ret_list[i].copy=strcpy(av[i],ft_strlen(av[i]));
+		i = 0;
+		while (str[i])
+			i++;
+		write(1, str, i);
 	}
-	ret_list[i].str = 0
-	return (ret_list);
+	write(1, "\n", 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].str)
+	{
+		ft_putstring(par[i].str);
+		ft_putnbr(par[i].size);
+		write(1, "\n", 1);
+		ft_putstring(par[i].copy);
+		i++;
+	}
 }
